2945a0a10906bb456a031b2c33d8bf7c8c5fc22e
Anne Caroline <annianinha@hotmail.com>
2022-09-19 16:25:47 -0300

    primeiro commit

---
+++ CadastroCliente/Models/Produtos.cs
@@ -0,0 +1,17 @@
+﻿namespace CadastroCliente.Models
+    {
+    public class Produtos
+
+        {
+         public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+
+        public Produtos(string descricao)
+            {
+            Descricao = descricao;
+            }
+
+        public decimal PrecoUnitario { get; set; }
+        }
+    }
