a50a1eea8642be76dcbd01c1976d2c8dc30e23ef
Anne Caroline <annianinha@hotmail.com>
2022-09-21 11:58:41 -0300

    models de pedidos,produtos e pedidoitem

--- CadastroCliente/Models/Produtos.cs
+++ CadastroCliente/Models/Produtos.cs
@@ -1,15 +1,21 @@
-﻿namespace CadastroCliente.Models
+﻿using System;
+
+namespace CadastroCliente.Models
     {
     public class Produtos
 
         {
+        private DateTime dataFabricação;
+
         public int Id { get; set; }
         public string Nome { get; set; }
         public string Descricao { get; set; }
-            
+        public string PreçoUnitario { get; set; }
+        public System.DateTime DataFabricação { get => dataFabricação; set => dataFabricação = value; }
+        public string Garantia { get; set; }
         }
 
-      
 
-      
-        }
+
+
+    }
--- CadastroCliente/Models/pedidos.cs
+++ CadastroCliente/Models/pedidos.cs
@@ -2,5 +2,15 @@
     {
     public class pedidos
         {
+        private string numeroPedido;
+        public int Id { get; set; }
+        public string ClienteID { get; set; }
+        public string NumeroPedido { get => numeroPedido; set => numeroPedido = value; }
+        public System.DateTime DataCompra { get; set; }
+
+        public System.DateTime DataEntrega { get; set; }
+        public string ValorTotal { get; set; }
+
+
         }
     }
